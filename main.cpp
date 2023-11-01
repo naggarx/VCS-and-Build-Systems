cea85f5fdba7ad0808f69e0cc24bed2eb4a7fbc2
naggarx <m.m.a.2002@live.com>
2023-11-01 12:07:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ int getSum(vector<int> nums)
     return sum;
     
 }
+int getAverage(vector<int> nums)
+{
+    int siz = nums.size();
+    int sum = getSum(nums);
+    return sum / siz;
+}
 int main()
 {
     vector<int> nums {1,2,3,4,5,6,7,8,9,10};
