1d6380a0079c25c1b3f77fe4885310a53c9aa319
naggarx <m.m.a.2002@live.com>
2023-11-01 12:12:17 +0200

    min

--- main.cpp
+++ main.cpp
@@ -14,8 +14,20 @@ int getSum(vector<int> nums)
     return sum;
     
 }
+int getMin(vector<int> nums)
+{
+    if(nums.size() == 0)
+        return 0;
+    int mini = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+        mini = min(mini , nums[i]);
+    }
+    return mini;
+}
 int main()
 {
     vector<int> nums {1,2,3,4,5,6,7,8,9,10};
-    cout<<getSum(nums);
+    cout<<getSum(nums)<<endl;
+    cout<<getMin(nums);
 }
